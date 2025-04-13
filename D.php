f02843844b1fb7191a98f37e322cebdd99c8dddf
Davi Pedro <davisilvaps02@gmail.com>
2025-04-13 10:42:15 -0300

    Criação da Classe D

---
+++ D.php
@@ -0,0 +1,35 @@
+<?php
+
+class D {
+
+    private integer $D1;
+    private float $D2;
+
+    //constructor
+    public function __construct(integer $D1, float $D2) {
+        $this->D1 = $D1;
+        $this->D2 = $D2;
+    }
+
+    public function getD1(): integer {
+        return $this->D1;
+    }
+    public function getD2(): float {
+        return $this->D2;
+    }
+
+    public function setD1(integer $D1): void {
+        $this->D1 = $D1;
+    }
+    public function setD2(float $D2): void {
+        $this->D2 = $D2;
+    }
+
+    public function MD1() {
+        echo __METHOD__ . "\n";
+    }
+
+    public function MD2() {
+        echo __METHOD__ . "\n";
+    }
+}
