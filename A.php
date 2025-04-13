0d1da4f959256f9610ad8cfbe7dde602b66da485
Davi Pedro <davisilvaps02@gmail.com>
2025-04-13 09:37:51 -0300

    feat: criação das classes A e B

---
+++ A.php
@@ -0,0 +1,35 @@
+<?php
+
+class A {
+
+    private integer $A1;
+    private float $A2;
+
+    //constructor
+    public function __construct(integer $A1, float $A2) {
+        $this->A1 = $A1;
+        $this->A2 = $A2;
+    }
+
+    public function getA1(): integer {
+        return $this->A1;
+    }
+    public function getA2(): float {
+        return $this->A2;
+    }
+
+    public function setA1(integer $A1): void {
+        $this->A1 = $A1;
+    }
+    public function setA2(float $A2): void {
+        $this->A2 = $A2;
+    }
+
+    public function MA1() {
+        echo __METHOD__ . "\n";
+    }
+
+    public function MA2() {
+        echo __METHOD__ . "\n";
+    }
+}
---
+++ B.php
@@ -0,0 +1,35 @@
+<?php
+
+class B {
+
+    private integer $B1;
+    private float $B2;
+
+    //constructor
+    public function __construct(integer $B1, float $B2) {
+        $this->B1 = $B1;
+        $this->B2 = $B2;
+    }
+
+    public function getB1(): integer {
+        return $this->B1;
+    }
+    public function getB2(): float {
+        return $this->B2;
+    }
+
+    public function setB1(integer $B1): void {
+        $this->B1 = $B1;
+    }
+    public function setB2(float $B2): void {
+        $this->B2 = $B2;
+    }
+
+    public function MB1() {
+        echo __METHOD__ . "\n";
+    }
+
+    public function MB2() {
+        echo __METHOD__ . "\n";
+    }
+}
